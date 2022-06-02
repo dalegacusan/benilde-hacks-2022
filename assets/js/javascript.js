e9c144dd170f635033ec8d58cc10ec9c852903a2
Dale Gacusan <dale@xpay.world>
2022-06-03 02:59:30 +0800

    Finished home page details

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,23 +1,16 @@
-function storage(){
+function storage() {
     var productname = document.getElementById('productname');
-    localStorage.setItem("name", productname.value);
+    localStorage.setItem('name', productname.value);
 
-    const ListFood = localStorage.getItem("name");
-    alert("Successfully Added To The Product Choices!");
+    alert('Successfully Added To The Product Choices!');
 }
 
-
-let optionsfood = ()=>{
-    console.log("asdasd");
+let optionsfood = () => {
+    console.log('asdasd');
     var select = document.getElementById('ListFood');
-    const ListFood = localStorage.getItem("name");
+    const ListFood = localStorage.getItem('name');
     var opt = document.createElement('option');
     opt.value = ListFood;
     opt.innerHTML = ListFood;
-    select.appendChild(opt)
-
-    console.log(opt);
-    console.log(ListFood);
-}
-
-console.log("asde")
+    select.appendChild(opt);
+};
